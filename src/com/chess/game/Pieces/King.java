d5f4d8f53e9c0c0c56fb74bf7d36204ea200232d
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2023-09-30 01:10:00 +0100

    completed moves logic for the king piece class

---
+++ src/com/chess/game/Pieces/King.java
@@ -0,0 +1,53 @@
+package com.chess.game.Pieces;
+
+import com.chess.game.Board.Board;
+import com.chess.game.Board.BoardUtils;
+import com.chess.game.Board.Move;
+import com.chess.game.Board.Tile;
+import com.chess.game.PieceColor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class King extends Piece{
+
+    int [] POSSIBLE_MOVES_COORDINATE = {-9, -8, -7, -1, 1, 7, 8, 9};
+    public King(int piecePosition, PieceColor pieceColor) {
+        super(piecePosition, pieceColor);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        List<Move> legalMoves = new ArrayList<>();
+
+        for (int moveOffset : POSSIBLE_MOVES_COORDINATE){
+            int destinationCoodinate = this.piecePosition + moveOffset;
+            if (isAtEighthColumnExcludedPosition(this.piecePosition, moveOffset) ||isAtFirstColumnExcludedPosition(this.piecePosition, moveOffset)){
+                continue;
+            }
+            if(BoardUtils.isValidCoordinate(destinationCoodinate)){
+                Tile targetTile = Board.getTile(destinationCoodinate);
+                Piece pieceAtTargetTile = targetTile.getPiece();
+
+                if(!targetTile.isTileOccupied()){
+                    legalMoves.add(new Move.NonAttackMove(board, this, destinationCoodinate));
+                }
+                else {
+                    if(this.pieceColor != pieceAtTargetTile.getPieceColor()){
+                        legalMoves.add(new Move.AttackMove(board, this, destinationCoodinate, pieceAtTargetTile));
+                    }
+                }
+            }
+        }
+
+        return legalMoves;
+    }
+
+    public static boolean isAtFirstColumnExcludedPosition(int currentPos, int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPos] && (candidateOffset == 7 || candidateOffset == -1 || candidateOffset == -9);
+    }
+    public static boolean isAtEighthColumnExcludedPosition(int currentPos, int candidateOffset){
+        return BoardUtils.EIGHTH_COLUMN[currentPos] && (candidateOffset == 9 || candidateOffset == 1 || candidateOffset == -7);
+    }
+}
